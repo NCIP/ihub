f90f05997c2662db000bf6e3f456c1003f37c4c6
Ajay Nalamala <analamala@ekagrasoft.com>
2010-11-08 19:47:13 +0000

    Added new files and updated package naming.
    SVN-Revision: 40

---
+++ software/mirth-components/java-src/gov/nih/nci/ihub/writer/ncies/common/GridMessage.java
@@ -0,0 +1,25 @@
+package gov.nih.nci.ihub.writer.ncies.common;
+
+import java.util.List;
+
+import org.w3c.dom.Element;
+
+public interface GridMessage {
+
+	public Element getMetaData();
+
+	public Element getPayload();
+
+	public List<Element> getPayloads();
+
+	public Element getSchemaDefinition();
+
+	public String getExternalIdentifier();
+
+	public String getCaxchangeIdentifier();
+
+	public String getOperationName();
+
+	public String getServiceType();
+
+}
