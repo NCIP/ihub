edbd6e98386216807e9bd5d33dbca5001cf9229c
Ajay Nalamala <analamala@ekagrasoft.com>
2010-11-10 20:11:09 +0000

    Renamed the file and added delegated credential reference functionality.
    SVN-Revision: 42

--- software/mirth-components/java-src/gov/nih/nci/ihub/test/CaGridAuthenticationManagerTest.java
+++ software/mirth-components/java-src/gov/nih/nci/ihub/test/CaGridAuthenticationManagerTest.java
@@ -1,42 +1,46 @@
 package gov.nih.nci.ihub.test;
 
+import org.apache.log4j.Logger;
+
 import static org.junit.Assert.fail;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import gov.nih.nci.cagrid.opensaml.SAMLAssertion;
 import gov.nih.nci.ihub.writer.ncies.exception.AuthenticationConfigurationException;
 import gov.nih.nci.ihub.writer.ncies.exception.AuthenticationErrorException;
-import gov.nih.nci.ihub.writer.ncies.infrastructure.GetSubjectUsingCaGridCredentialsBean;
+import gov.nih.nci.ihub.writer.ncies.infrastructure.CaGridAuthenticationManager;
 
-import org.globus.gsi.GlobusCredential;
-import org.globus.gsi.GlobusCredentialException;
+import org.apache.log4j.BasicConfigurator;
 import org.junit.Test;
 
-public class GetSubjectUsingCaGridCredentialsTest {
+public class CaGridAuthenticationManagerTest {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger
+		.getLogger(CaGridAuthenticationManagerTest.class);
 
 	@Test
-	public void testGetSubjectUsingCaGridCredentials() {		 
-		GetSubjectUsingCaGridCredentialsBean subjectUsingCaGridCredentialsBean = new GetSubjectUsingCaGridCredentialsBean(
+	public void testGetSubjectUsingCaGridCredentials() {
+		BasicConfigurator.configure();
+
+		/*CaGridAuthenticationManager caGridAuthenticationManager = new CaGridAuthenticationManager(
 				"coppagridtest",
 				"Coppa#12345",
 				"https://cagrid-dorian-stage.nci.nih.gov:8443/wsrf/services/cagrid/Dorian",
 				"https://cagrid-dorian-stage.nci.nih.gov:8443/wsrf/services/cagrid/Dorian",
+				"https://cagrid-cds-stage.nci.nih.gov:8443/wsrf/services/cagrid/CredentialDelegationService",
+				12);*/
+		
+		CaGridAuthenticationManager caGridAuthenticationManager = new CaGridAuthenticationManager(
+				"ccts1amz@ekagra",
+				"Ccts@mz123!",
+				"https://dorian.training.cagrid.org:8443/wsrf/services/cagrid/Dorian",
+				"https://dorian.training.cagrid.org:8443/wsrf/services/cagrid/Dorian",
+				"https://cds.training.cagrid.org:8443/wsrf/services/cagrid/CredentialDelegationService",
 				12);
+		
 		try {
-			SAMLAssertion saAssertion = subjectUsingCaGridCredentialsBean
-					.authenticate(
-							"https://cagrid-dorian-stage.nci.nih.gov:8443/wsrf/services/cagrid/Dorian",
-							"coppagridtest", "Coppa#12345");
-
-			System.out.println("SAML Assertion successfully obtained");
-			// Obtained the GlobusCredential for the Authenticated User
-			GlobusCredential userCredential = subjectUsingCaGridCredentialsBean
-					.obtainProxy(saAssertion);			
-			System.out.println("Identity: " + userCredential.getIdentity());
-			
+			System.out.println("testGetSubjectUsingCaGridCredentials() - Delegated Credential Reference: "
+						+ caGridAuthenticationManager
+							.getDelegatedCredentialReference());	
 			
 			
 			/*ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
@@ -95,6 +99,8 @@ public class GetSubjectUsingCaGridCredentialsTest {
 		} catch (AuthenticationErrorException e) {
 			fail(e.getMessage());
 		} catch (AuthenticationConfigurationException e) {
+			fail(e.getMessage());
+		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 	}
