b7b19453364a8161a7d55209ee28811f4177c921
Ajay Nalamala <analamala@ekagrasoft.com>
2010-12-01 02:00:09 +0000

    Several changes made. Some new files and some deletions.
    SVN-Revision: 106

--- software/mirth-components/java-src/gov/nih/nci/ihub/test/GenericInvocationStrategyTest.java
+++ software/mirth-components/java-src/gov/nih/nci/ihub/test/GenericInvocationStrategyTest.java
@@ -26,21 +26,46 @@ public class GenericInvocationStrategyTest {
 			Subject subject = delegationServiceBean.invokeDelegationService();
 			System.out.println("SUBJECT: "+subject.toString());
 			
-			GenericInvocationStrategy targetInvocationStrategy = new gov.nih.nci.ihub.writer.ncies.common.GenericInvocationStrategy();
-			targetInvocationStrategy.setServiceUrl(TestConstants.REGISTER_SUBJECT_CAAERS_URL);
-			targetInvocationStrategy.setGridClientClassName("gov.nih.nci.ccts.grid.client.RegistrationConsumerClient");
-			targetInvocationStrategy.setRequestPayloadClassName("gov.nih.nci.cabig.ccts.domain.Registration");
-			targetInvocationStrategy.setReturnTypeNameSpace("gme://ccts.cabig/1.0/gov.nih.nci.cabig.ccts.domain");
-			targetInvocationStrategy.setReturnTypeElement("registration");
-						
-			Element ihubBusinessMessagePayloadDocument = IntegrationHubUtil.
-			stringToDOMDocument(TestConstants.REGISTER_SUBJECT_PAYLOAD).getDocumentElement();
-			targetInvocationStrategy.setStrategySpecificVariables("register", 
-									"REGISTER_SUBJECT", ihubBusinessMessagePayloadDocument, 
-									subject, "caAERS");
-			GridInvocationResult gridInvocationResult = targetInvocationStrategy.invokeGridService(false);
-			System.out.println("Grid Invocation Result: "+IntegrationHubUtil.xmlToString(gridInvocationResult.getResult()));
 			
+			GenericInvocationStrategy studyCreationStrategy = new gov.nih.nci.ihub.writer.ncies.common.GenericInvocationStrategy();
+			studyCreationStrategy.setServiceType("STUDY_CREATION");
+			studyCreationStrategy.setServiceUrl(TestConstants.STUDY_CREATION_CAAERS_URL);
+			studyCreationStrategy.setGridClientClassName("gov.nih.nci.ccts.grid.studyconsumer.client.StudyConsumerClient");
+			studyCreationStrategy.setRequestPayloadClassName("gov.nih.nci.cabig.ccts.domain.Study");
+			studyCreationStrategy.setReturnTypeNameSpace("gme://ccts.cabig/1.0/gov.nih.nci.cabig.ccts.domain");
+			studyCreationStrategy.setReturnTypeElement("study");	
+			studyCreationStrategy.setOperationName("createStudy");
+						
+			Element studyCreationPayloadDocument = IntegrationHubUtil.
+			stringToDOMDocument(TestConstants.STUDY_CREATION_PAYLOAD).getDocumentElement();
+			
+			studyCreationStrategy.setSubject(subject);
+			studyCreationStrategy.setServiceProviderName("caAERS");
+			studyCreationStrategy.setPayload(studyCreationPayloadDocument);
+			
+			GridInvocationResult studyCreationResponse = studyCreationStrategy.invokeGridService(false);
+			System.out.println("Study Creation Response: "+IntegrationHubUtil.xmlToString(studyCreationResponse.getResult()));
+			
+			/*
+			GenericInvocationStrategy registrationInvocationStrategy = new gov.nih.nci.ihub.writer.ncies.common.GenericInvocationStrategy();
+			registrationInvocationStrategy.setServiceType("REGISTER_SUBJECT");
+			registrationInvocationStrategy.setServiceUrl(TestConstants.REGISTER_SUBJECT_CAAERS_URL);
+			registrationInvocationStrategy.setGridClientClassName("gov.nih.nci.ccts.grid.client.RegistrationConsumerClient");
+			registrationInvocationStrategy.setRequestPayloadClassName("gov.nih.nci.cabig.ccts.domain.Registration");
+			registrationInvocationStrategy.setReturnTypeNameSpace("gme://ccts.cabig/1.0/gov.nih.nci.cabig.ccts.domain");
+			registrationInvocationStrategy.setReturnTypeElement("registration");	
+			registrationInvocationStrategy.setOperationName("register");
+						
+			Element subjectRegistrationPayloadDocument = IntegrationHubUtil.
+			stringToDOMDocument(TestConstants.REGISTER_SUBJECT_PAYLOAD).getDocumentElement();
+			
+			registrationInvocationStrategy.setSubject(subject);
+			registrationInvocationStrategy.setServiceProviderName("caAERS");
+			registrationInvocationStrategy.setPayload(subjectRegistrationPayloadDocument);
+			
+			GridInvocationResult subjectRegistrationResponse = registrationInvocationStrategy.invokeGridService(false);
+			System.out.println("Subject Registration Response: "+IntegrationHubUtil.xmlToString(subjectRegistrationResponse.getResult()));
+			*/
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail(e.getMessage());
