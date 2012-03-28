c9e8c7399113a28b9e68bcdee5dea1a4c9bdde61
Vinodh Chandrasekaran <vinodh.rc@semanticbits.com>
2012-03-28 13:30:04 +0000

    delete catissue folder
    SVN-Revision: 275

--- software/projects/transcend-catissue/src/main/java/gov/nih/nci/integration/catissue/CaTissueClient.java
+++
@@ -1,8 +0,0 @@
-package gov.nih.nci.integration.catissue;
-
-public interface CaTissueClient {
-	
-	public void createObject(String xmlStr, String operationName) throws InvokeCaTissueException, PayloadParseException,Exception;
-
-
-}
