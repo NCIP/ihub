f62a1940a925f86f6636dbe8d8730b461c6e2f4c
Vinodh Chandrasekaran <vinodh.rc@semanticbits.com>
2012-04-13 15:35:32 +0000

    created a util class for JAXBMarshaller and added some tests
    SVN-Revision: 299

---
+++ projects/common/src/main/java/gov/nih/nci/integration/util/JAXBMarshaller.java
@@ -0,0 +1,33 @@
+package gov.nih.nci.integration.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+/**
+ * Marshals any object to xml based on its context
+ * @author vinodh.rc@semanticbits.com
+ *
+ */
+public class JAXBMarshaller {
+
+    /**
+     * Marshals Object to its xml format, based on its context
+     * @param obj - object to be marshalled
+     * @return marshalled string
+     * @throws JAXBException exception thrown, if any
+     */
+    public String marshal(Object obj) throws JAXBException {
+        final JAXBContext jc = JAXBContext.newInstance(obj.getClass());
+        
+        final Marshaller m = jc.createMarshaller();
+        final StringWriter sw = new StringWriter();
+        final PrintWriter pw = new PrintWriter(sw);
+        m.marshal(obj, pw);
+        
+        return sw.toString();
+    }
+}
--- projects/common/src/test/java/gov/nih/nci/integration/transformer/XSLTTransformerTest.java
+++ projects/common/src/test/java/gov/nih/nci/integration/transformer/XSLTTransformerTest.java
@@ -1,8 +1,15 @@
 package gov.nih.nci.integration.transformer;
 
 import gov.nih.nci.integration.exception.IntegrationException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +25,35 @@ public class XSLTTransformerTest {
 	private XSLTTransformer xsltTransformer;
 	
 	@Test
+	@Before
 	public void intialize() throws IntegrationException {
-		xsltTransformer.initTransformer("MsgBroadcasterParticipant-to-caTissueParticipant.xsl", "C:\\Users\\sb-admin-cp\\.integration\\ihub\\xsl\\");
+		xsltTransformer.initTransformer("MsgBroadcasterParticipant-to-caTissueParticipant.xsl", "D:\\Workspace\\Suite-iHub\\software\\projects\\transcend-catissue\\src\\main\\resources\\xsl\\");
 		Assert.assertNotNull(xsltTransformer);
 	}
 	
+	
+	@Test
+	public void transform() throws IntegrationException , FileNotFoundException, IOException{
+		
+		InputStream in = XSLTTransformerTest.class.getResourceAsStream("MsgBroadcasterParticipant.xml"); 
+
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(in));	 
+//		String line=null;
+//		while ((line = br.readLine()) != null) {
+//			System.out.println(line);
+//		}
+//		br.close();
+		
+		ByteArrayOutputStream baos = null; 
+		baos = new ByteArrayOutputStream();
+
+		
+		xsltTransformer.transform(null, in, baos);
+		
+		Assert.assertNotNull(xsltTransformer);
+		
+		
+	}
 }
+
