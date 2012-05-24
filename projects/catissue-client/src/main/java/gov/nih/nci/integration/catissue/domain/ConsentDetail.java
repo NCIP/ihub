530db05c67410c5decc0a58e24d643f09a76b25d
Rohit Gupta <rohit.gupta@semanticbits.com>
2012-05-24 13:37:22 +0000

    Adding domain classes for RegisterConsent flow
    SVN-Revision: 371

---
+++ projects/catissue-client/src/main/java/gov/nih/nci/integration/catissue/domain/ConsentDetail.java
@@ -0,0 +1,42 @@
+package gov.nih.nci.integration.catissue.domain;
+
+import edu.wustl.catissuecore.domain.CollectionProtocol;
+
+/**
+ * 
+ * @author Rohit Gupta
+ * 
+ */
+public class ConsentDetail {
+
+	private String collectionProtocolEvent;
+
+	private ConsentData consentData;
+
+	private CollectionProtocol collectionProtocol;
+
+	public String getCollectionProtocolEvent() {
+		return collectionProtocolEvent;
+	}
+
+	public void setCollectionProtocolEvent(String collectionProtocolEvent) {
+		this.collectionProtocolEvent = collectionProtocolEvent;
+	}
+
+	public ConsentData getConsentData() {
+		return consentData;
+	}
+
+	public void setConsentData(ConsentData consentData) {
+		this.consentData = consentData;
+	}
+
+	public CollectionProtocol getCollectionProtocol() {
+		return collectionProtocol;
+	}
+
+	public void setCollectionProtocol(CollectionProtocol collectionProtocol) {
+		this.collectionProtocol = collectionProtocol;
+	}
+
+}
