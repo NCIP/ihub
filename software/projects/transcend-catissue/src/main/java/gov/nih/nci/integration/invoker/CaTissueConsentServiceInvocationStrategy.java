0138fd3b10f781a4b58edccb305493a398f0a9cb
Rohit Gupta <rohit.gupta@semanticbits.com>
2012-05-30 21:32:50 +0000

    changes for ErrorCode mapping
    SVN-Revision: 394

--- software/projects/transcend-catissue/src/main/java/gov/nih/nci/integration/invoker/CaTissueConsentServiceInvocationStrategy.java
+++ software/projects/transcend-catissue/src/main/java/gov/nih/nci/integration/invoker/CaTissueConsentServiceInvocationStrategy.java
@@ -3,13 +3,20 @@ package gov.nih.nci.integration.invoker;
 import gov.nih.nci.integration.catissue.CaTissueConsentClient;
 import gov.nih.nci.integration.domain.ServiceInvocationMessage;
 import gov.nih.nci.integration.domain.StrategyIdentifier;
+import gov.nih.nci.integration.exception.IntegrationError;
 import gov.nih.nci.integration.exception.IntegrationException;
 import gov.nih.nci.integration.transformer.XSLTTransformer;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +35,7 @@ public class CaTissueConsentServiceInvocationStrategy implements ServiceInvocati
 
 	private XSLTTransformer xsltTransformer;
 	
+	Map<String, IntegrationError> msgToErrMap;	
 	
 	public CaTissueConsentServiceInvocationStrategy(int retryCount,
 			CaTissueConsentClient caTissueSpecimenClient,
@@ -36,6 +44,11 @@ public class CaTissueConsentServiceInvocationStrategy implements ServiceInvocati
 		this.retryCount = retryCount;
 		this.caTissueConsentClient = caTissueSpecimenClient;
 		this.xsltTransformer = xsltTransformer;
+		
+		HashMap<String, IntegrationError> msgToErrMapBase = new LinkedHashMap<String, IntegrationError>();
+		msgToErrMapBase.put("Specimen for given LABEL doesn't exist", IntegrationError._1090);
+		
+		msgToErrMap = Collections.synchronizedMap(msgToErrMapBase);
 	}
 
 	@Override
@@ -61,19 +74,15 @@ public class CaTissueConsentServiceInvocationStrategy implements ServiceInvocati
 		} catch (IntegrationException e) {
 			serviceInvocationResult.setInvocationException(e);
 		} 
+		
+		handleException(serviceInvocationResult);
 		return serviceInvocationResult;
 	}
 
 	@Override
 	public ServiceInvocationResult rollback(ServiceInvocationMessage msg) {
-		ServiceInvocationResult serviceInvocationResult = new ServiceInvocationResult();
-		
-		try {			
-			
-			System.out.println("Inside Consent Strategy..  Rollback().. msg.getOriginalData() --> " + msg.getOriginalData());
-//			String consentListXMLStr = transformToConsentXML(msg.getMessage().getRequest());		
-//			System.out.println("Inside Consent Strategy..  Rollback().. consentListXMLStr --> " + consentListXMLStr);
-			
+		ServiceInvocationResult serviceInvocationResult = new ServiceInvocationResult();		
+		try {					
 			String consentListXMLStr = msg.getOriginalData();
 			
 			// call the method to rollback Specimens
@@ -115,4 +124,52 @@ public class CaTissueConsentServiceInvocationStrategy implements ServiceInvocati
 		return specimenXMLStr;
 	}
 
+	
+	private void handleException(ServiceInvocationResult result) {
+		if(!result.isFault()) {
+			return;
+		}
+		
+		Exception exception = result.getInvocationException();
+		Throwable cause = exception;
+		while(cause instanceof IntegrationException) {
+			cause = cause.getCause();
+		}
+		if(cause == null){
+			return;
+		}
+		
+		String[] throwableMsgs = getThrowableMsgs(cause);
+		IntegrationException newie = (IntegrationException) exception;
+		
+		Set<String> keys = msgToErrMap.keySet();
+		
+		for (String lkupStr : keys) {
+			String msg = getMatchingMsg(lkupStr, throwableMsgs);			
+			if(msg != null) {
+				newie = new IntegrationException(msgToErrMap.get(lkupStr), cause, msg );				
+				break;
+			}
+		}
+		
+		result.setInvocationException(newie);
+	}
+
+	private String[] getThrowableMsgs(Throwable cause) {
+		Throwable[] throwables = ExceptionUtils.getThrowables(cause);
+		String[] msgs = new String[throwables.length];
+		for (int i = 0; i < throwables.length; i++) {
+			msgs[i] = throwables[i].getMessage();
+		}
+		return msgs;
+	}
+	
+	private String getMatchingMsg(String lookupStr, String[] msgs) {
+		for (String string : msgs) {
+			if(string.contains(lookupStr)) {
+				return string;
+			}
+		}
+		return null;
+	}
 }
--- software/projects/transcend-catissue/src/main/java/gov/nih/nci/integration/invoker/CaTissueUpdateSpecimenServiceInvocationStrategy.java
+++ software/projects/transcend-catissue/src/main/java/gov/nih/nci/integration/invoker/CaTissueUpdateSpecimenServiceInvocationStrategy.java
@@ -3,13 +3,20 @@ package gov.nih.nci.integration.invoker;
 import gov.nih.nci.integration.catissue.CaTissueSpecimenClient;
 import gov.nih.nci.integration.domain.ServiceInvocationMessage;
 import gov.nih.nci.integration.domain.StrategyIdentifier;
+import gov.nih.nci.integration.exception.IntegrationError;
 import gov.nih.nci.integration.exception.IntegrationException;
 import gov.nih.nci.integration.transformer.XSLTTransformer;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +35,7 @@ public class CaTissueUpdateSpecimenServiceInvocationStrategy implements ServiceI
 
 	private XSLTTransformer xsltTransformer;
 	
+	Map<String, IntegrationError> msgToErrMap;	
 	
 	public CaTissueUpdateSpecimenServiceInvocationStrategy(int retryCount,
 			CaTissueSpecimenClient caTissueSpecimenClient,
@@ -36,6 +44,22 @@ public class CaTissueUpdateSpecimenServiceInvocationStrategy implements ServiceI
 		this.retryCount = retryCount;
 		this.caTissueSpecimenClient = caTissueSpecimenClient;
 		this.xsltTransformer = xsltTransformer;
+		
+		HashMap<String, IntegrationError> msgToErrMapBase = new LinkedHashMap<String, IntegrationError>();
+		
+		msgToErrMapBase.put("Specimen with the same LABEL already exists", IntegrationError._1080);
+		msgToErrMapBase.put("Specimen Type is invalid", IntegrationError._1081);
+		msgToErrMapBase.put("Tissue Side is invalid", IntegrationError._1082);
+		msgToErrMapBase.put("Tissue Site is invalid", IntegrationError._1083);
+		msgToErrMapBase.put("Specimen Collection Group not found in caTissue", IntegrationError._1084);	
+		msgToErrMapBase.put("Available Quantity cannot be greater than the Initial Quantity", IntegrationError._1085);
+		msgToErrMapBase.put("Pathological Status is invalid", IntegrationError._1086);
+		msgToErrMapBase.put("Collection Protocol Event can't be changed while updating the Specimen", IntegrationError._1087);
+		msgToErrMapBase.put("Collection Protocol can't be changed while updating the Specimen", IntegrationError._1088);
+		msgToErrMapBase.put("Specimen Class can't be changed while updating the Specimen", IntegrationError._1089);
+		msgToErrMapBase.put("Specimen for given LABEL doesn't exist", IntegrationError._1090);
+		
+		msgToErrMap = Collections.synchronizedMap(msgToErrMapBase);
 	}
 
 	@Override
@@ -64,6 +88,8 @@ public class CaTissueUpdateSpecimenServiceInvocationStrategy implements ServiceI
 		} catch (Exception e) {
 			serviceInvocationResult.setInvocationException(e);
 		}
+		
+		handleException(serviceInvocationResult);
 		return serviceInvocationResult;
 	}
 
@@ -110,6 +136,54 @@ public class CaTissueUpdateSpecimenServiceInvocationStrategy implements ServiceI
 			}
 		}
 		return specimenXMLStr;
+	}
+	
+	private void handleException(ServiceInvocationResult result) {
+		if(!result.isFault()) {
+			return;
+		}
+		
+		Exception exception = result.getInvocationException();
+		Throwable cause = exception;
+		while(cause instanceof IntegrationException) {
+			cause = cause.getCause();
+		}
+		if(cause == null){
+			return;
+		}
+		
+		String[] throwableMsgs = getThrowableMsgs(cause);
+		IntegrationException newie = (IntegrationException) exception;
+		
+		Set<String> keys = msgToErrMap.keySet();
+		
+		for (String lkupStr : keys) {
+			String msg = getMatchingMsg(lkupStr, throwableMsgs);			
+			if(msg != null) {
+				newie = new IntegrationException(msgToErrMap.get(lkupStr), cause, msg );				
+				break;
+			}
+		}
+		
+		result.setInvocationException(newie);
+	}
+
+	private String[] getThrowableMsgs(Throwable cause) {
+		Throwable[] throwables = ExceptionUtils.getThrowables(cause);
+		String[] msgs = new String[throwables.length];
+		for (int i = 0; i < throwables.length; i++) {
+			msgs[i] = throwables[i].getMessage();
+		}
+		return msgs;
+	}
+	
+	private String getMatchingMsg(String lookupStr, String[] msgs) {
+		for (String string : msgs) {
+			if(string.contains(lookupStr)) {
+				return string;
+			}
+		}
+		return null;
 	}
 
 }
