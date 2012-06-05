04d4f92be7bf6b5359a19caffbe9fa935c4daa56
Rohit Gupta <rohit.gupta@semanticbits.com>
2012-06-05 21:22:35 +0000

    PMD Fix
    SVN-Revision: 478

--- software/projects/common/src/main/java/gov/nih/nci/integration/util/ErrorMessagesUtil.java
+++ software/projects/common/src/main/java/gov/nih/nci/integration/util/ErrorMessagesUtil.java
@@ -4,6 +4,9 @@ import java.net.MalformedURLException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Util class to get the error message for the error code for the current locale
  * 
@@ -13,6 +16,8 @@ import java.util.ResourceBundle;
 public final class ErrorMessagesUtil {
 
     private static ResourceBundle rb = null;
+
+    private static final Logger LOG = LoggerFactory.getLogger(ErrorMessagesUtil.class);
 
     private ErrorMessagesUtil() {
         // private constructor
@@ -26,10 +31,11 @@ public final class ErrorMessagesUtil {
      */
     public static void init(String msgsLocation) {
         try {
-            CustomUrlClassLoader ccl = new CustomUrlClassLoader(
-                    CustomUrlClassLoader.class.getClassLoader().getParent(), msgsLocation);
+            final CustomUrlClassLoader ccl = new CustomUrlClassLoader(CustomUrlClassLoader.class.getClassLoader()
+                    .getParent(), msgsLocation);
             rb = ResourceBundle.getBundle("messages.errorMessages", Locale.getDefault(), ccl);
         } catch (MalformedURLException e) {
+            LOG.error("Error while loading the errorMessages.properties file. ", e);
         }
     }
 
@@ -41,7 +47,7 @@ public final class ErrorMessagesUtil {
      */
     public static String getMessage(String errorCode) {
         // if ResourceBundle is not loaded correctly load the default one in the classpath
-        if (rb == null) {
+        if (rb == null) { // NOPMD
             rb = ResourceBundle.getBundle("messages.errorMessages");
         }
         return rb.getString(errorCode);
