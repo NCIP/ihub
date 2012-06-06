94c2ee3b3fac4c22757e4a7e8346dafc04a899ad
Rohit Gupta <rohit.gupta@semanticbits.com>
2012-06-06 21:32:48 +0000

    Checkstyle fixes
    SVN-Revision: 485

--- projects/catissue-client/src/main/java/gov/nih/nci/integration/catissue/domain/ConsentData.java
+++ projects/catissue-client/src/main/java/gov/nih/nci/integration/catissue/domain/ConsentData.java
@@ -5,37 +5,58 @@ import java.util.Set;
 
 import edu.wustl.catissuecore.domain.ConsentTierStatus;
 
-/**
- * 
- * @author Rohit Gupta
- * 
+/** 
+ * @author Rohit Gupta 
  */
 public class ConsentData {
 
-    Set<ConsentTierStatus> consentTierStatusSet;
-
-    public void setConsentTierStatusSet(Set<ConsentTierStatus> consentTierStatusSet) {
-        this.consentTierStatusSet = consentTierStatusSet;
-    }
-
+    private Set<ConsentTierStatus> consentTierStatusSet;
     private String specimenLabel;
 
+    /**
+     * Constructor
+     */
     public ConsentData() {
         consentTierStatusSet = new LinkedHashSet<ConsentTierStatus>();
     }
 
+    /**
+     * Set the ConsentTierStatusSet
+     * 
+     * @param consentTierStatusSet - Set<ConsentTierStatus>
+     */
+    public void setConsentTierStatusSet(Set<ConsentTierStatus> consentTierStatusSet) {
+        this.consentTierStatusSet = consentTierStatusSet;
+    }
+
+    /**
+     * Get SpecimenLabel
+     * @return SpecimenLabel
+     */
     public String getSpecimenLabel() {
         return specimenLabel;
     }
 
+    /**
+     * Set SpecimenLabel
+     * @param specimenLabel - SpecimenLabel
+     */
     public void setSpecimenLabel(String specimenLabel) {
         this.specimenLabel = specimenLabel;
     }
 
+    /**
+     * Add a ConsentTierStatus to the consentTierStatusSet
+     * @param consentTierStatus - object to be added in the Set
+     */
     public void add(ConsentTierStatus consentTierStatus) {
         consentTierStatusSet.add(consentTierStatus);
     }
 
+    /**
+     * To get ConsentTierStatusSet
+     * @return Set<ConsentTierStatus>
+     */
     public Set<ConsentTierStatus> getConsentTierStatusSet() {
         return consentTierStatusSet;
     }
--- projects/catissue-client/src/main/java/gov/nih/nci/integration/catissue/domain/ConsentDetail.java
+++ projects/catissue-client/src/main/java/gov/nih/nci/integration/catissue/domain/ConsentDetail.java
@@ -3,38 +3,58 @@ package gov.nih.nci.integration.catissue.domain;
 import edu.wustl.catissuecore.domain.CollectionProtocol;
 
 /**
- * 
  * @author Rohit Gupta
- * 
  */
 public class ConsentDetail {
 
     private String collectionProtocolEvent;
-
     private ConsentData consentData;
-
     private CollectionProtocol collectionProtocol;
 
+    /**
+     * Get collectionProtocolEvent
+     * @return collectionProtocolEventId
+     */
     public String getCollectionProtocolEvent() {
         return collectionProtocolEvent;
     }
 
+    /**
+     * Set collectionProtocolEvent 
+     * @param collectionProtocolEvent - collectionProtocolEventId
+     */
     public void setCollectionProtocolEvent(String collectionProtocolEvent) {
         this.collectionProtocolEvent = collectionProtocolEvent;
     }
 
+    /**
+     * To get ConsentData
+     * @return ConsentData object
+     */
     public ConsentData getConsentData() {
         return consentData;
     }
 
+    /**
+     * Set ConsentData
+     * @param consentData - ConsentData to be set
+     */
     public void setConsentData(ConsentData consentData) {
         this.consentData = consentData;
     }
 
+    /**
+     * To get CollectionProtocol
+     * @return CollectionProtocol
+     */
     public CollectionProtocol getCollectionProtocol() {
         return collectionProtocol;
     }
 
+    /**
+     * Set CollectionProtocol
+     * @param collectionProtocol - CollectionProtocol
+     */
     public void setCollectionProtocol(CollectionProtocol collectionProtocol) {
         this.collectionProtocol = collectionProtocol;
     }
