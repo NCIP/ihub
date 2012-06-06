b10b419a64bcc215e201624631992f6269245789
Rohit Gupta <rohit.gupta@semanticbits.com>
2012-06-06 21:42:21 +0000

    Checkstyle fixes
    SVN-Revision: 494

--- software/projects/transcend-caaers/src/main/java/gov/nih/nci/integration/caaers/invoker/CaAERSServiceInvocationStrategyFactory.java
+++ software/projects/transcend-caaers/src/main/java/gov/nih/nci/integration/caaers/invoker/CaAERSServiceInvocationStrategyFactory.java
@@ -3,19 +3,26 @@ package gov.nih.nci.integration.caaers.invoker;
 import gov.nih.nci.integration.invoker.ServiceInvocationStrategy;
 import gov.nih.nci.integration.util.CustomClasspathXmlApplicationContext;
 
-import java.util.Arrays;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.Executors;
 
-public class CaAERSServiceInvocationStrategyFactory {
+/**
+ * Factory class for Strategy for caAERS
+ * @author Vinodh
+ *
+ */
+public final class CaAERSServiceInvocationStrategyFactory {
 
     private static ServiceInvocationStrategy caaersRegistrationServiceInvocationStrategy = null;
 
     private static ServiceInvocationStrategy caaersUpdateRegistrationServiceInvocationStrategy = null;
 
     private static Boolean initStatus = null;
+    
+    private CaAERSServiceInvocationStrategyFactory() {        
+    }
 
     private static synchronized void init(final String[] caaersLibLocation, final String... caaersConfig) {
         ExecutorCompletionService<Boolean> ecs = new ExecutorCompletionService<Boolean>(Executors
@@ -48,8 +55,15 @@ public class CaAERSServiceInvocationStrategyFactory {
         }
     }
 
+    /**
+     * Method to get caaersRegistrationServiceInvocationStrategy
+     * @param caaersLibLocation - caaersLibLocation
+     * @param caaersConfig - caaersConfig
+     * @return caaersRegistrationServiceInvocationStrategy
+     */
+    // CHECKSTYLE:OFF
     public static ServiceInvocationStrategy createCaAERSRegistrationServiceInvocationStrategy(
-            final String caaersLibLocation[], final String... caaersConfig) {
+            final String caaersLibLocation[] , final String... caaersConfig) {
 
         if (initStatus == null && caaersRegistrationServiceInvocationStrategy == null) {
             init(caaersLibLocation, caaersConfig);
@@ -61,6 +75,12 @@ public class CaAERSServiceInvocationStrategyFactory {
         }
     }
 
+    /**
+     * Method to get caaersUpdateRegistrationServiceInvocationStrategy
+     * @param caaersLibLocation - caaersLibLocation
+     * @param caaersConfig - caaersConfig
+     * @return caaersUpdateRegistrationServiceInvocationStrategy
+     */
     public static ServiceInvocationStrategy createCaAERSUpdateRegistrationServiceInvocationStrategy(
             final String caaersLibLocation[], final String... caaersConfig) {
 
