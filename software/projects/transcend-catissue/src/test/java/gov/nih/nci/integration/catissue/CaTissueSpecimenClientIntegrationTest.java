b1e2e0e3046bf0c1e0864702774fa13aa66c271a
Rohit Gupta <rohit.gupta@semanticbits.com>
2012-06-06 21:47:15 +0000

    Checkstyle fixes
    SVN-Revision: 497

--- software/projects/transcend-catissue/src/test/java/gov/nih/nci/integration/catissue/CaTissueSpecimenClientIntegrationTest.java
+++ software/projects/transcend-catissue/src/test/java/gov/nih/nci/integration/catissue/CaTissueSpecimenClientIntegrationTest.java
@@ -10,9 +10,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
+ * IntegrationTest class for Specimen
  * 
  * @author Rohit Gupta
- * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath*:applicationContext-transcend-catissue.xml")
@@ -21,6 +21,9 @@ public class CaTissueSpecimenClientIntegrationTest {
     @Autowired
     private CaTissueSpecimenClient caTissueSpecimenClient;
 
+    /**
+     * Testcase for createSpecimen
+     */
     @Test
     public void createSpecimen() {
         ServiceInvocationResult svc = caTissueSpecimenClient.createSpecimens(getCreateSpecimenXMLStr());
@@ -28,13 +31,17 @@ public class CaTissueSpecimenClientIntegrationTest {
 
     }
 
-    // @Test
+    /**
+     * Testcase for updateSpecimen
+     */
+    @Test
     public void updateSpecimen() {
         ServiceInvocationResult svc = caTissueSpecimenClient.updateSpecimens(getUpdateSpecimenXMLStr());
         assertNotNull(svc);
 
     }
 
+    // CHECKSTYLE:OFF
     private String getCreateSpecimenXMLStr() {
         // return"<?xml version=\"1.0\" ?><specimens><participant><lastName>66604232</lastName><activityStatus>Active</activityStatus></participant><specimenDetail><specimen class=\"TissueSpecimen\"><initialQuantity>6.0</initialQuantity><pathologicalStatus>Malignant</pathologicalStatus><specimenClass>Tissue</specimenClass><specimenType>Fixed Tissue</specimenType><specimenCharacteristics><tissueSide>Right</tissueSide><tissueSite>Placenta</tissueSite></specimenCharacteristics><activityStatus>Active</activityStatus><availableQuantity>4.0</availableQuantity><barcode>TolvenTestUser252TissueSpecimen19</barcode><label>TolvenTestUser252TissueSpecimen19</label><isAvailable>true</isAvailable><collectionStatus>Collected</collectionStatus></specimen><collectionProtocol><title>6482</title><shortTitle>6482</shortTitle></collectionProtocol></specimenDetail><specimenDetail><specimen class=\"FluidSpecimen\"><initialQuantity>8.0</initialQuantity><pathologicalStatus>Not Specified</pathologicalStatus><specimenClass>Fluid</specimenClass><specimenType>Not Specified</specimenType><specimenCharacteristics><tissueSide>Not Specified</tissueSide><tissueSite>Not Specified</tissueSite></specimenCharacteristics><activityStatus>Active</activityStatus><availableQuantity>2.0</availableQuantity><barcode>TolvenTestUser252TissueSpecimen29</barcode><label>TolvenTestUser252TissueSpecimen29</label><isAvailable>true</isAvailable><collectionStatus>Collected</collectionStatus></specimen><collectionProtocol><title>6482</title><shortTitle>6482</shortTitle></collectionProtocol></specimenDetail></specimens>";
 
