41121f3fc03bcbe5697d4a840800cbd220169c87
Rohit Gupta <rohit.gupta@semanticbits.com>
2012-06-08 17:09:46 +0000

    Static Analysis fixes
    SVN-Revision: 525

--- projects/caaers-client/src/test/java/gov/nih/nci/integration/caaers/CaAERSRegistrationServiceInvocationStrategyIntegrationTest.java
+++ projects/caaers-client/src/test/java/gov/nih/nci/integration/caaers/CaAERSRegistrationServiceInvocationStrategyIntegrationTest.java
@@ -33,71 +33,71 @@ public class CaAERSRegistrationServiceInvocationStrategyIntegrationTest {
 
     @Autowired
     private CaAERSRegistrationServiceInvocationStrategy caAersRegistrationServiceInvocationStrategy;
-    
+
     @Autowired
     private CaAERSUpdateRegistrationServiceInvocationStrategy caAersUpdateRegistrationServiceInvocationStrategy;
 
     @Autowired
     private ServiceInvocationMessageDao serviceInvocationMessageDao;
 
-    private final Long refMsgId = 12345L;
-    
+    private static final Long REFMSGID = 12345L;
+
     /**
-     * Tests createParticipant using the ServiceInvocationStrategy class. 
+     * Tests createParticipant using the ServiceInvocationStrategy class.
      */
     @Test
     public void createParticipant() {
 
-        ServiceBroadcaster sb = new DefaultServiceBroadcaster(serviceInvocationMessageDao);
-        sb.delegateServiceInvocation(refMsgId, getInvalidPStr(), caAersRegistrationServiceInvocationStrategy);
+        final ServiceBroadcaster sb = new DefaultServiceBroadcaster(serviceInvocationMessageDao);
+        sb.delegateServiceInvocation(REFMSGID, getInvalidPStr(), caAersRegistrationServiceInvocationStrategy);
 
-        Map<StrategyIdentifier, ServiceInvocationMessage> msgsMap = serviceInvocationMessageDao
-                .getAllByReferenceMessageId(refMsgId);
+        final Map<StrategyIdentifier, ServiceInvocationMessage> msgsMap = serviceInvocationMessageDao
+                .getAllByReferenceMessageId(REFMSGID);
 
         Assert.assertNotNull(msgsMap);
         Assert.assertFalse(msgsMap.isEmpty());
 
-        ServiceInvocationMessage resultMsg = msgsMap.get(caAersRegistrationServiceInvocationStrategy
+        final ServiceInvocationMessage resultMsg = msgsMap.get(caAersRegistrationServiceInvocationStrategy
                 .getStrategyIdentifier());
         Assert.assertNotNull(resultMsg);
         Assert.assertNotNull(resultMsg.getInvocationException());
 
-        ServiceInvocationResult result = caAersRegistrationServiceInvocationStrategy.rollback(resultMsg);
+        final ServiceInvocationResult result = caAersRegistrationServiceInvocationStrategy.rollback(resultMsg);
         Assert.assertNotNull(result);
         Assert.assertTrue(result.isFault());
     }
-    
+
     /**
-     * Tests createParticipant using the ServiceInvocationStrategy class. 
+     * Tests createParticipant using the ServiceInvocationStrategy class.
      */
     @Test
     public void updateParticipant() {
 
-        ServiceBroadcaster sb = new DefaultServiceBroadcaster(serviceInvocationMessageDao);
-        sb.delegateServiceInvocation(refMsgId, getInvalidPStr(), caAersUpdateRegistrationServiceInvocationStrategy);
+        final ServiceBroadcaster sb = new DefaultServiceBroadcaster(serviceInvocationMessageDao);
+        sb.delegateServiceInvocation(REFMSGID, getInvalidPStr(), caAersUpdateRegistrationServiceInvocationStrategy);
 
-        Map<StrategyIdentifier, ServiceInvocationMessage> msgsMap = serviceInvocationMessageDao
-                .getAllByReferenceMessageId(refMsgId);
+        final Map<StrategyIdentifier, ServiceInvocationMessage> msgsMap = serviceInvocationMessageDao
+                .getAllByReferenceMessageId(REFMSGID);
 
         Assert.assertNotNull(msgsMap);
         Assert.assertFalse(msgsMap.isEmpty());
 
-        ServiceInvocationMessage resultMsg = msgsMap.get(caAersUpdateRegistrationServiceInvocationStrategy
+        final ServiceInvocationMessage resultMsg = msgsMap.get(caAersUpdateRegistrationServiceInvocationStrategy
                 .getStrategyIdentifier());
         Assert.assertNotNull(resultMsg);
         Assert.assertNotNull(resultMsg.getInvocationException());
-        
-        //to test rollback set original data
+
+        // to test rollback set original data
         resultMsg.setOriginalData(getInvalidPStr());
 
-        ServiceInvocationResult result = caAersUpdateRegistrationServiceInvocationStrategy.rollback(resultMsg);
+        final ServiceInvocationResult result = caAersUpdateRegistrationServiceInvocationStrategy.rollback(resultMsg);
         Assert.assertNotNull(result);
         Assert.assertTrue(result.isFault());
     }
 
- // CHECKSTYLE:OFF
+    // CHECKSTYLE:OFF
     private String getInvalidPStr() {
         return "<?xml version=\"1.0\"?><caaers:participant xmlns:caaers=\"http://webservice.caaers.cabig.nci.nih.gov/participant\" xmlns:p=\"http://integration.nci.nih.gov/participant\" id=\"1\" version=\"1\"><firstName>Cherry0415</firstName><lastName>Blossom0415</lastName><maidenName/><middleName/><birthDate>1965-11-24</birthDate><gender>Male</gender><race>White</race><ethnicity>Not Hispanic or Latino</ethnicity><identifiers><caaers:organizationAssignedIdentifier id=\"1\" version=\"1\"><type>MRN</type><value>997025</value><primaryIndicator>true</primaryIndicator><caaers:organization id=\"1\" version=\"1\"><name>QU</name><nciInstituteCode>DCP</nciInstituteCode></caaers:organization></caaers:organizationAssignedIdentifier><caaers:systemAssignedIdentifier id=\"1\" version=\"1\"><type>MRN</type><value>997025</value><primaryIndicator>true</primaryIndicator><systemName>MRN</systemName></caaers:systemAssignedIdentifier></identifiers><assignments><caaers:assignment id=\"1\" version=\"1\"><studySubjectIdentifier>48824</studySubjectIdentifier><caaers:studySite id=\"1\" version=\"1\"><caaers:study id=\"1\" version=\"1\"><identifiers><identifier id=\"1\" version=\"1\"><type>Protocol Authority Identifier</type><value>6482</value></identifier></identifiers></caaers:study><caaers:organization id=\"1\" version=\"1\"><name>QU</name><nciInstituteCode>DCP</nciInstituteCode></caaers:organization></caaers:studySite></caaers:assignment></assignments></caaers:participant>";
     }
- // CHECKSTYLE:ON
+    // CHECKSTYLE:ON
 }
