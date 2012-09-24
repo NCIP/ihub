0215eb45fa588550313da59d9b1a5ff2d8832f1e
Rohit Gupta <rohit.gupta@semanticbits.com>
2012-09-24 21:42:29 +0000

    Static Analysis fixes.
    SVN-Revision: 921

--- software/projects/common/src/main/java/gov/nih/nci/integration/validation/ValidationConfig.java
+++ software/projects/common/src/main/java/gov/nih/nci/integration/validation/ValidationConfig.java
@@ -10,18 +10,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+/**
+ * This class is used to read the configuration for schematron validation
+ * 
+ * @author Vinodh
+ * 
+ */
 @Configuration
 public class ValidationConfig {
-    
+
     @Value("${ihub.validation.schematron.extract.failures.xsl}")
     private String extractFailuresSchematronXsl;
-    
+
     @Value("${integration.transformer.xsl.baseClassPath}")
     private String xslBaseClassPath;
-    
+
     @Autowired
-    private ApplicationContext context; 
-    
+    private ApplicationContext context;
+
     /**
      * Schematron Validator
      * 
@@ -31,10 +37,10 @@ public class ValidationConfig {
     @Bean
     @Scope("prototype")
     public SchematronValidator schematronValidator() throws IntegrationException {
-        XSLTTransformer extractFailuresTransformer = context.getBean(XSLTTransformer.class);
+        final XSLTTransformer extractFailuresTransformer = context.getBean(XSLTTransformer.class);
         extractFailuresTransformer.initTransformer(extractFailuresSchematronXsl, xslBaseClassPath);
-        
-        XSLTTransformer evaluateRulesTransformer = context.getBean(XSLTTransformer.class);        
+
+        final XSLTTransformer evaluateRulesTransformer = context.getBean(XSLTTransformer.class);
         return new SchematronValidator(evaluateRulesTransformer, extractFailuresTransformer, xslBaseClassPath);
     }
 }
