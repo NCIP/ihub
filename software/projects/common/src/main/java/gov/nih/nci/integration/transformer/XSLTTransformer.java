890e21635176bbbbd69153a89588b12360096766
ksambhuni <kumar.sambhuni@semanticbits.com>
2013-05-22 23:47:01 -0400

    Add copyright header to JAVA, JSP, XML and Properties files

--- software/projects/common/src/main/java/gov/nih/nci/integration/transformer/XSLTTransformer.java
+++ software/projects/common/src/main/java/gov/nih/nci/integration/transformer/XSLTTransformer.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright Ekagra and SemanticBits, LLC
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/ihub/LICENSE.txt for details.
+ */
 package gov.nih.nci.integration.transformer;
 import gov.nih.nci.integration.exception.IntegrationError;
 import gov.nih.nci.integration.exception.IntegrationException;
--- software/projects/common/src/main/java/gov/nih/nci/integration/validation/SchematronValidator.java
+++ software/projects/common/src/main/java/gov/nih/nci/integration/validation/SchematronValidator.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright Ekagra and SemanticBits, LLC
+ * 
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See http://ncip.github.com/ihub/LICENSE.txt for details.
+ */
 package gov.nih.nci.integration.validation;
 
 import gov.nih.nci.integration.exception.IntegrationError;
